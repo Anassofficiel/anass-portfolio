a92e0128213789deb1676e8d8a59aa119fbb56be
Anassofficiel <fatiihanass99@gmail.com>
2026-08-12 18:41:33 +0100

    fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,27 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
   plugins: [
     tanstackStart(),
+
+    ...(process.env.VERCEL
+      ? [
+        nitro({
+          preset: "vercel",
+        }),
+      ]
+      : []),
+
     react(),
     tailwindcss(),
   ],
